cba5ad9ae0059bc110f858bfdf19122d81c77a73
Connor Kobriger <kobrigercj2180@aguafria.org>
2018-08-31 10:56:57 -0700

    filled rectangle

--- shapes/shapes.js
+++ shapes/shapes.js
@@ -4,3 +4,8 @@
 var can = document.getElementById('myCanvas');
 // then use the variable to get the "context";
 var ctx = can.getContext('2d');
+// Now we are ready to start drawing
+//Lets start with rectangles
+
+//This makes a filled rectangle
+ctx.fillRect(25, 25, 100, 100)
