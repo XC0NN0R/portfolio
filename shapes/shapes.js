d587b59ba5ce44b2969a571d2576fe7d12ef78c8
Connor Kobriger <kobrigercj2180@aguafria.org>
2018-08-31 10:54:45 -0700

    added shapes folder w/ shapes.html & shapes.js

---
+++ shapes/shapes.js
@@ -0,0 +1,6 @@
+// In this program we will be drawing basic shapes
+
+// first make a variable for the canvas
+var can = document.getElementById('myCanvas');
+// then use the variable to get the "context";
+var ctx = can.getContext('2d');
