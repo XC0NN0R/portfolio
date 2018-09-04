273b286b759366cac32fd92257f870d10180bcb5
Connor Kobriger <kobrigercj2180@aguafria.org>
2018-09-04 10:34:52 -0700

    Created arcs.js and removed circles.js

---
+++ shapes/arcs.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.linewidth = 4
+ctx.strokeStyle = "red";
+
+
+ctx. beginPath();
+ctx.arc(50, 50, 20, 0, Math.PI / 2, false);
+//(x, y, radius, starting angle, stopping angle, direction)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(100, 50, 20, 0, Math.PI / 2, false);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(150, 50, 20, 0, Math.PI * 2, false);
+ctx.stroke();
